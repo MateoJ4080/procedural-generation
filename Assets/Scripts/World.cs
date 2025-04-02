eae34c5d102a07a58df3280c22f8a364c7530f76
Mate93934 <mateodjm@gmail.com>
2025-04-02 20:34:12 -0300

    Feat (world-gen): add Perlin noise terrain generation
    - Implemented Perlin noise to generate terrain height dynamically.

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -13,32 +13,40 @@ public class World : MonoBehaviour
 
     void GenerateWorld()
     {
+        float noiseScale = 0.1f;
+        int maxHeight = height;
+
         blocks = new Block[width, height, depth];
 
+        // **1. Crear bloques sin generar la malla**
         for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int z = 0; z < depth; z++)
             {
-                for (int z = 0; z < depth; z++)
+                int terrainHeight = Mathf.FloorToInt(Mathf.PerlinNoise(x * noiseScale, z * noiseScale) * maxHeight);
+
+                for (int y = 0; y < height; y++)
                 {
                     GameObject blockObj = Instantiate(blockPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
                     Block block = blockObj.GetComponent<Block>();
                     block.position = new Vector3Int(x, y, z);
+                    block.blockType = y <= terrainHeight ? Block.BlockType.Solid : Block.BlockType.Air;
                     blocks[x, y, z] = block;
                 }
             }
         }
 
-        // Generate mesh after all blocks are instantiated to avoid unnecesary faces-rendering
+        // **2. Generar las mallas**
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
                 for (int z = 0; z < depth; z++)
                 {
-                    if (blocks[x, y, z].TryGetComponent<BlockMeshGenerator>(out var meshGen))
+                    Block block = blocks[x, y, z];
+                    if (block.blockType == Block.BlockType.Solid && block.TryGetComponent<BlockMeshGenerator>(out var meshGen))
                     {
-                        meshGen.Block = blocks[x, y, z];
+                        meshGen.Block = block;
                         meshGen.World = this;
                         meshGen.GenerateMesh();
                     }
@@ -46,6 +54,7 @@ public class World : MonoBehaviour
             }
         }
     }
+
     public void RefreshAdjacentBlocks()
     {
 
