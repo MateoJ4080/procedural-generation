faa448f4651c925c14fa8199cc364cf5b75d42cd
Mate93934 <mateodjm@gmail.com>
2025-12-03 07:20:08 -0300

    refactor: add PlayerAuthoring to Player as first step of its conversion to entity
    - Player is being converted to an entity so it can interact with chunk colliders generated through Unity Physics, since a GO cannot collide with entities because they run on different physics engines (PhysX vs Unity Physics).

---
+++ Assets/Scripts/Player/PlayerAuthoring.cs
@@ -0,0 +1,21 @@
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class PlayerAuthoring : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+            AddComponent<PlayerTag>(entity);
+            AddComponent<LocalTransform>(entity);
+            AddComponent<PlayerMoveInput>(entity);
+            AddComponent(entity, new PlayerSpeed { Value = authoring.speed });
+        }
+    }
+}
