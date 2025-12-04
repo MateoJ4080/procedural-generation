eb5801872f59585d0976325f13a0d41d85906ebb
Mate93934 <mateodjm@gmail.com>
2025-12-04 07:14:02 -0300

    feat: add PlayerInputBridge

---
+++ Assets/Scripts/Player/PlayerInputBridge.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public class PlayerInputBridge : MonoBehaviour
+{
+    public InputSystem_Actions controls;
+
+    private EntityManager em;
+    private Entity playerEntity;
+
+    void Awake()
+    {
+        em = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        controls = new InputSystem_Actions();
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Start()
+    {
+        playerEntity = em.CreateEntityQuery(typeof(PlayerTag)).GetSingletonEntity();
+    }
+
+    void Update()
+    {
+        float2 moveInput = (float2)controls.Player.Move.ReadValue<Vector2>();
+        em.SetComponentData(playerEntity, new PlayerMoveInput { Value = moveInput });
+    }
+}
