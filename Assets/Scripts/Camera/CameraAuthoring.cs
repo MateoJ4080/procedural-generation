16ebc5a91b4a6cf113a101e779d0ca2bb7c4b0c5
Mate93934 <mateodjm@gmail.com>
2025-12-09 08:23:37 -0300

    feat: add CameraConfig to camera entity

--- Assets/Scripts/Camera/CameraAuthoring.cs
+++ Assets/Scripts/Camera/CameraAuthoring.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class CameraAuthoring : MonoBehaviour
 {
+    public float Sensitivity = 2f;
+    public float PitchMin = -89f;
+    public float PitchMax = 89f;
+
     public class Baker : Baker<CameraAuthoring>
     {
         public override void Bake(CameraAuthoring authoring)
         {
-            var entity = GetEntity(TransformUsageFlags.Dynamic);
-
+            var camEntity = GetEntity(TransformUsageFlags.Dynamic);
             var parent = GetEntity(authoring.transform.parent, TransformUsageFlags.Dynamic);
 
-            AddComponent(entity, new Parent { Value = parent });
-            AddComponent<PostTransformMatrix>(entity);
+            AddComponent(camEntity, new Parent { Value = parent });
+            AddComponent<CameraTag>(camEntity);
+            AddComponent<CameraLookInput>(camEntity);
+            AddComponent(camEntity, new CameraConfig
+            {
+                Sensitivity = authoring.Sensitivity,
+                PitchMin = authoring.PitchMin,
+                PitchMax = authoring.PitchMax
+            });
         }
     }
 }
