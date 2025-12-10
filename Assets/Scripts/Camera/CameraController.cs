c4d882c9b6a19760742dc3062dfc717985bed83b
Mate93934 <mateodjm@gmail.com>
2025-12-10 14:37:10 -0300

    feat: reimplement CameraController for GO

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,11 +1,13 @@
+using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float sensitivity = 0.5f;
-    [SerializeField] private Transform player;
 
-    float xRotation = 0;
+    private float pitch = 0;
+    private float yaw = 0;
 
     private InputSystem_Actions controls;
 
@@ -25,10 +27,11 @@ public class CameraController : MonoBehaviour
         Vector2 lookInput = controls.Player.Look.ReadValue<Vector2>();
         lookInput *= sensitivity * 0.01f;
 
-        xRotation -= lookInput.y;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        pitch -= lookInput.y;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.Rotate(Vector3.up * lookInput.x);
+        yaw += lookInput.x;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, 0f);
     }
 }
