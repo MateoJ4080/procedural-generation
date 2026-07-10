ecf353cbfcd08128e95ae6db8c9538f9238491f4
Mate93934 <mateodjm@gmail.com>
2026-07-10 00:51:53 -0300

    refactor: separate ChunkHeightJob into its own script

--- Assets/Scripts/Terrain/ChunkGenerationSystem.cs
+++ Assets/Scripts/Terrain/ChunkGenerationSystem.cs
@@ -1,4 +1,3 @@
-using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -68,14 +67,12 @@ public partial struct ChunkGenerationSystem : ISystem
                     state.EntityManager.AddSharedComponent(entity, new PhysicsWorldIndex(0));
 
                     var blocks = new NativeArray<Block>(16 * 16 * 16, Allocator.TempJob);
-
                     var chunkHeightJob = new ChunkHeightJob
                     {
                         chunkCoord = chunkCoord,
                         config = terrainConfig,
                         blocks = blocks
                     };
-
                     chunkHeightJob.Schedule().Complete();
 
                     var buffer = state.EntityManager.AddBuffer<Block>(entity);
@@ -141,45 +138,5 @@ public partial struct ChunkGenerationSystem : ISystem
         }
 
         _chunks.Clear();
-    }
-}
-
-[BurstCompile]
-public struct ChunkHeightJob : IJob
-{
-    public int2 chunkCoord;
-    public TerrainConfig config;
-    public NativeArray<Block> blocks;
-
-    public void Execute()
-    {
-        for (int x = 0; x < 16; x++)
-        {
-            for (int z = 0; z < 16; z++)
-            {
-                float nx = (x + chunkCoord.x * 16) * config.Frequency;
-                float nz = (z + chunkCoord.y * 16) * config.Frequency;
-
-                float height = 0f;
-                float amp = config.Amplitude;
-                float freq = 1f;
-
-                for (int o = 0; o < config.Octaves; o++)
-                {
-                    height += amp * noise.snoise(new float2(nx * freq, nz * freq));
-                    amp *= config.Persistence;
-                    freq *= config.Lacunarity;
-                }
-
-                height = height * 0.5f + 0.5f;
-                int maxY = (int)math.floor(height * 16);
-
-                for (int y = 0; y < 16; y++)
-                {
-                    int index = x + y * 16 + z * 16 * 16;
-                    blocks[index] = new Block { Type = (byte)(y <= maxY ? 1 : 0) };
-                }
-            }
-        }
     }
 }
